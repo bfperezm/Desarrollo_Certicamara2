c5a839b10153b348f2678b76906a015c039beb45
bfperezm <fermetal01@gmail.com>
2012-04-09 23:50:50 -0500

    first commit

---
+++ NegativosNoPermitidos.java
@@ -0,0 +1,8 @@
+
+public class NegativosNoPermitidos extends Exception {
+	 public NegativosNoPermitidos(String negativo) {
+		 super("Los numeros negativos no son validos "+negativo+",");
+	}
+	        
+	    
+}
